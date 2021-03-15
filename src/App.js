05ada22eb6aed3a700f2bb468ac9cde357dc0f10
Jest13 <camil.hirane@ynov.com>
2021-03-15 21:47:52 +0100

    connexion api

--- src/App.js
+++ src/App.js
@@ -1,53 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import CardGroup from "react-bootstrap/CardGroup";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
 function App() {
+  const [latest, setLatest] = useState("")
+
+
+  // connexion api 
+  useEffect(() => {
+    axios
+    .get("https://corona.lmao.ninja/v3/covid-19/all")
+    .then(res => {
+    console.log(res.data);
+    })
+    .catch(err => {
+     console.log(err);
+    });
+  }, []);
+
   return (
-   <div>
-     <CardGroup>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+    <div>
+      <CardGroup>
+        <Card bg="warning">
+          <Card.Body>
+            <Card.Title>Cas</Card.Title>
+            <Card.Text>
+              test
       </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="danger">
+          <Card.Body>
+            <Card.Title>Mort(s)</Card.Title>
+            <Card.Text>
+              0
       </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="success">
+          <Card.Body>
+            <Card.Title>Case</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This card has even longer content than the first to
+              show that equal height action.
       </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
     </div>
   );
 }
