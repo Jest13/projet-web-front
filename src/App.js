0e439f3005ca06488cf2046464b87027ad93ec57
Jest13 <camil.hirane@ynov.com>
2021-03-21 00:45:35 +0100

    creation du filtre de recherche

--- src/App.js
+++ src/App.js
@@ -3,67 +3,79 @@ import React, { useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import CardGroup from "react-bootstrap/CardGroup";
 import CardColumns from "react-bootstrap/CardColumns";
-
+import Form from 'react-bootstrap/Form'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import axios from "axios";
 
 function App() {
   const [latest, setLatest] = useState([]);
   const [results, setResults] = useState([]);
+  const [searchCountries, setSearchCountries] = useState("");
 
   // connexion api 
   useEffect(() => {
     axios
-    .all([
-    //donnees general
-    axios.get("https://corona.lmao.ninja/v3/covid-19/all"),
-    //donnes par pays 
-    axios.get("https://corona.lmao.ninja/v3/covid-19/countries")
-    ])
-    .then(responseArr => {
-    setLatest(responseArr[0].data);
-    setResults(responseArr[1].data);
-    })
-    .catch(err => {
-     console.log(err);
-    });
+      .all([
+        //donnees general
+        axios.get("https://corona.lmao.ninja/v3/covid-19/all"),
+        //donnes par pays 
+        axios.get("https://corona.lmao.ninja/v3/covid-19/countries")
+      ])
+      .then(responseArr => {
+        setLatest(responseArr[0].data);
+        setResults(responseArr[1].data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, []);
-// initialisation du timer update des donnees 
+  // initialisation du timer update des donnees 
 
 
-    const date = new Date(parseInt(latest.updated)); 
-    const lastUpdated = date.toString();
+  const date = new Date(parseInt(latest.updated));
+  const lastUpdated = date.toString();
+  const filterCountries = results.filter(item => {
+    return searchCountries !== "" ? item.country.includes(searchCountries) : item;
+  });
+  const countries = filterCountries.map((data, i) => {
+    return (
+      <Card
+        key={i}
+        bg="light"
+        text="dark"
+        className="text-center"
+        style={{ margin: "10px" }}
+      >
+        <Card.Img variant="top" src={data.countryInfo.flag} />
+        <Card.Body>
+          <Card.Title>{data.country}</Card.Title>
+          <Card.Text>Cas : {data.cases}</Card.Text>
+          <Card.Text>Mort(s) : {data.deaths}</Card.Text>
+          <Card.Text>Gueris : {data.recovered}</Card.Text>
+          <Card.Text>Cas aujourd'hui : {data.todayCases}</Card.Text>
+          <Card.Text>Mort(s) aujourd'hui : {data.todayDeaths}</Card.Text>
+          <Card.Text>Cas actif : {data.active}</Card.Text>
+          <Card.Text>Cas critique : (hospitalisé/réanimation) {data.critical}</Card.Text>
+          <Card.Text>Cas : {data.cases}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  });
 
-    const countries = results.map((data, i) => {
-      return (
-<Card
-key={i}
-bg="light"
-text="dark"
-className="text-center"
-style={{ margin: "10px"}}
->
-<Card.Img variant="top" src={data.countryInfo.flag} />
-<Card.Body>
-  <Card.Title>{data.country}</Card.Title>
-  <Card.Text>Cas : {data.cases}</Card.Text>
-  <Card.Text>Mort(s) : {data.deaths}</Card.Text>
-  <Card.Text>Gueris : {data.recovered}</Card.Text>
-  <Card.Text>Cas aujourd'hui : {data.todayCases}</Card.Text>
-  <Card.Text>Mort(s) aujourd'hui : {data.todayDeaths}</Card.Text>
-  <Card.Text>Cas actif : {data.active}</Card.Text>
-  <Card.Text>Cas critique : (hospitalisé/réanimation) {data.critical}</Card.Text>
-  <Card.Text>Cas : {data.cases}</Card.Text>
-</Card.Body>
-</Card>
-);
-});
 
 
- 
-  
-//tableau des donnees
+
+  var queries = [{
+    columns: 2,
+    query: 'min-width: 500px'
+  }, {
+    columns: 3,
+    query: 'min-width: 1000px'
+  }];
+
+
+
+  //tableau des donnees
   return (
     <div>
       <CardGroup>
@@ -71,8 +83,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Cas</Card.Title>
             <Card.Text>
-             {latest.cases}
-      </Card.Text>
+              {latest.cases}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated} </small>
@@ -82,8 +94,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Mort(s)</Card.Title>
             <Card.Text>
-            {latest.deaths}
-      </Card.Text>
+              {latest.deaths}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated}</small>
@@ -93,8 +105,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Gueris</Card.Title>
             <Card.Text>
-             {latest.recovered}
-      </Card.Text>
+              {latest.recovered}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated}</small>
@@ -107,8 +119,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Cas aujourd'hui</Card.Title>
             <Card.Text>
-            {latest.todayCases}
-      </Card.Text>
+              {latest.todayCases}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated}</small>
@@ -118,8 +130,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Mort(s) aujourd'hui</Card.Title>
             <Card.Text>
-            {latest.todayDeaths}
-      </Card.Text>
+              {latest.todayDeaths}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated}</small>
@@ -129,8 +141,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Gueris aujourd'hui</Card.Title>
             <Card.Text>
-            {latest.todayRecovered}
-      </Card.Text>
+              {latest.todayRecovered}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated}</small>
@@ -145,8 +157,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Cas toujours actif</Card.Title>
             <Card.Text>
-            {latest.active}
-      </Card.Text>
+              {latest.active}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated}</small>
@@ -156,8 +168,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Cas critique</Card.Title>
             <Card.Text>
-            {latest.critical}
-      </Card.Text>
+              {latest.critical}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated}</small>
@@ -167,8 +179,8 @@ style={{ margin: "10px"}}
           <Card.Body>
             <Card.Title>Test effectué(s)</Card.Title>
             <Card.Text>
-            {latest.tests}
-      </Card.Text>
+              {latest.tests}
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Derniere mise à jour: {lastUpdated}</small>
@@ -176,10 +188,19 @@ style={{ margin: "10px"}}
         </Card>
       </CardGroup>
 
-      <CardColumns>
-      {countries}
-      </CardColumns>
-      
+      <Form>
+        <Form.Group controlId="formGroup">
+          <Form.Control 
+          type="text" 
+          placeholder="Chercher un pays"
+          onChange={e => setSearchCountries(e.target.value)} />
+        </Form.Group>
+      </Form>
+
+      <CardColumns queries={queries}>
+        {countries}
+       </CardColumns>
+
     </div>
   );
 }
